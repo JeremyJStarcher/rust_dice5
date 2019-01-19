2979d849791ec96d85bcfd3e9d01aa5e46ef1811
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-01-18 21:37:25 -0500

    Made code a little more 'functional' style

--- src/dice.rs
+++ src/dice.rs
@@ -1,32 +1,52 @@
 extern crate rand;
 use rand::Rng;
 
+pub type DieFace = i8;
+
 #[derive(Debug)]
 pub struct Dice {
-    pub dice: Vec<i8>,
+    pub dice: Vec<DieFace>,
 }
 
 impl Dice {
     const NUMBER_OF_DICE: usize = 5;
     const NUMBER_OF_FACES: i8 = 6;
 
-    pub fn roll_die() -> i8 {
+    pub fn roll_die() -> DieFace {
         let mut rng = rand::thread_rng();
         rng.gen_range(0, Dice::NUMBER_OF_FACES) + 1
     }
 
-    pub fn roll_all() -> Vec<i8> {
-        let mut r = Vec::with_capacity(Dice::NUMBER_OF_DICE);
+    pub fn roll_all() -> Vec<DieFace> {
+        let dice = Dice::get_empty_hand();
 
-        for _i in 0..Dice::NUMBER_OF_DICE {
-            let die = Dice::roll_die();
-            r.push(die);
-        }
+        let reroll_flags: Vec<bool> = dice.iter().map(|_i| true).collect();
+        println!("reroll_flags {:?}", reroll_flags);
+        let dice = Dice { dice };
 
-        return r;
+        let hew_hand = Dice::reroll_hand(dice, reroll_flags);
+        return hew_hand.dice;
     }
 
-    pub fn get_empty_hand() -> Vec<i8> {
+    pub fn reroll_hand(hand: Dice, reroll: Vec<bool>) -> Dice {
+        if hand.dice.len() != reroll.len() {
+            panic!("dice.length and re-roll length must match");
+        }
+
+        let dice: Vec<DieFace> = hand
+            .dice
+            .iter()
+            .zip(reroll)
+            .map(|(face, flag)| {
+                let f = if flag { Dice::roll_die() } else { *face };
+                f
+            })
+            .collect();
+
+        return Dice { dice };
+    }
+
+    pub fn get_empty_hand() -> Vec<DieFace> {
         let mut r = Vec::with_capacity(Dice::NUMBER_OF_DICE);
 
         for _i in 0..Dice::NUMBER_OF_DICE {
