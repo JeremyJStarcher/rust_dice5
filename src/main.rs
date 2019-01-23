2077d8b2d1b59fad13d03a0aeb9d8f34aecadc11
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-01-22 20:52:42 -0500

    Read input and loop in place

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,17 @@ mod dice;
 mod scorecard;
 mod ui;
 
+#[macro_use]
+extern crate text_io;
+
 fn main() {
-    let hand = dice::Dice::first_roll();
-    let reroll_flags: Vec<bool> = hand.dice.iter().map(|_i| true).collect();
-    println!("{}", hand);
-    println!("{:?}", calchand::calc_3k(&hand, false));
-
-    let hand2 = dice::Dice::reroll_hand(hand, reroll_flags);
-    println!("{}", hand2);
-    println!("{:?}", calchand::calc_3k(&hand2, false));
-
     let mut scorecard = scorecard::get_new_scorecard_data();
-    let points = calchand::calc_ace(&hand2, false);
 
-    let result = scorecard.set_val(&"1".to_string(), points);
-    match result {
-        Err(scorecard::SetError::NotFound) => {}
-        Err(scorecard::SetError::AlreadySet) => {}
-        Ok(_) => {}
+    while !scorecard.game_over() {
+        ui::show_card(&scorecard);
+
+        println!("Your Play >> ");
+        let line: String = read!("{}\n");
+        println!("You entered {} !", line);
     }
-
-    let points = calchand::calc_two(&hand2, false);
-    let result = scorecard.set_val(&"2".to_string(), points);
-    match result {
-        Err(scorecard::SetError::NotFound) => {}
-        Err(scorecard::SetError::AlreadySet) => {}
-        Ok(_) => {}
-    }
-    ui::show_card(&scorecard);
-    // println!("{}", scorecard);
-
-    // ui::demo();
 }
